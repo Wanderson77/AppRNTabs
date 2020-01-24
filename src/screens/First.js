043d1a4e3f7d47f7cbbbfd00e61b20c56f21aee8
Wanderson Aldo <wanderson.aldo@gmail.com>
2020-01-24 09:23:41 -0200

    Commit base

---
+++ src/screens/First.js
@@ -0,0 +1,16 @@
+/** Comments
+ * 
+ */
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+export default class FirstPage extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
---
+++ src/screens/Second.js
@@ -0,0 +1,15 @@
+/** Comments
+ * 
+ */
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class SecondPage extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Setting Screen</Text>
+      </View>
+    );
+  }
+}
